e2d4c6ad695e1c00b37bbf5b1316dcb36110942f
c_ymelpati <c_ymelpati@IM-PC-242.innomindshyd.com>
2018-02-02 15:06:48 +0530

    included the factory.
    The http call from the factory

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+var app = angular.module("myApp",[]);
 
-
-function studentController($scope,$http,$q) {
+function studentController($scope,$http,$q,myfactory,httpfactory) {
             var url = "data.txt";
 			var url1 = "data1.txt";
 			var url2 = "data2.txt";
 			var purl1 = "pdata1.txt";
 			var purl2 = "pdata2.txt";
+			
+			$scope.message = myfactory.showmessage();
+			console.log("my message"+$scope.message);
+			
+			// from the httpfactory
+			var handleSuccess = function(data, status) {
+				$scope.factstudents = data;
+				console.log("this is from factory"+$scope.factstudents);
+			};			
+			httpfactory.getSessions().success(handleSuccess);
+			
+			
             $http.get(url).then( function(response) {
                $scope.students = response.data;
             });
@@ -28,8 +40,27 @@ function studentController($scope,$http,$q) {
 			});
 			/** working with promises
 			**/
-			
-						
-			
-			
 	}
+	/** working with sample factory services. 
+	*/
+	app.factory("myfactory",function() {
+		var message="Hi this is from factory";
+		return {
+			showmessage: function(){
+				return message;
+			}
+			
+		};
+	});
+	// this factory is working successfully
+	app.factory("httpfactory",function($http){
+		var surl = "data.txt";
+		 return {
+        getSessions: function() {
+            return $http.get(surl, { 
+                type : 'getSource',
+                ID    : 'TP001'
+				});
+			}
+		};
+	});
